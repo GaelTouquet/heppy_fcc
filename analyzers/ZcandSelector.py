ec0bcf808da0272d3a8d633f302ff25ef4b9eee4
Gael <gael.touquet@etu.univ-lyon1.fr>
2015-06-18 11:18:05 +0200

    added from mu reconstruction

--- analyzers/ZcandSelector.py
+++ analyzers/ZcandSelector.py
@@ -3,26 +3,14 @@ from heppy.framework.analyzer import Analyzer
 class Zcandselector(Analyzer):
 
     def process(self, event):
+        zcand_types = self.cfg_ana.zcand_types
         zcand = []
-        zcand.extend(getattr(event,
-                             'zcandidates_from_es',
-                             []))
-        zcand.extend(getattr(event,
-                             'zcandidates_from_mus',
-                             []))
-        zcand.extend(getattr(event,
-                             'zcandidates_from_jets_Zcandidates',
-                             []))
-        zcand.extend(getattr(event,
-                             'zcandidates_from_smeared_es',
-                             []))
-        zcand.extend(getattr(event,
-                             'zcandidates_from_smeared_mus',
-                             []))
-        zcand.extend(getattr(event,
-                             'zcandidates_from_smeared_jets_Zcandidates',
-                             []))
+        for typ in zcand_types:
+            zcand.extend(getattr(event,
+                                 'zcandidates_from_{typ}'.format(typ=typ),
+                                 []))
+        
         sorted_zcand = sorted( zcand,
                                key = lambda x : abs(x.m() - 91.187621))
         if len(sorted_zcand)>0:
-            event.zcand = sorted_zcand[0]
+            event.zcands = sorted_zcand[0]
